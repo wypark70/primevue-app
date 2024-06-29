3192bad7ee8691e1d52e12f26044c136d693c870
wypark70 <wypark70@gmail.com>
2024-06-29 10:28:52 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# primevue-app
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/wypark70/primevue-app)
---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <ThemeSwitcher />
+    <div class="card">
+        <TreeTable v-model:selectionKeys="selectedKey" :value="nodes" selectionMode="checkbox" tableStyle="min-width: 50rem">
+            <Column field="name" header="Name" expander style="width: 34%"></Column>
+            <Column field="size" header="Size" style="width: 33%"></Column>
+            <Column field="type" header="Type" style="width: 33%"></Column>
+        </TreeTable>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { NodeService } from '@/service/NodeService';
+
+onMounted(() => {
+    NodeService.getTreeTableNodes().then((data) => (nodes.value = data));
+});
+
+const nodes = ref();
+const selectedKey = ref();
+</script>
